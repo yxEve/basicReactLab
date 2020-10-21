f73d582a231f1483f4302048ca6ba80242bf1cb1
Yuxin Nie <1371454048@qq.com>
2020-10-21 23:40:31 +0100

    Exercise 4 completed

--- components/exercises/04_iteration.js
+++ components/exercises/04_iteration.js
@@ -2,9 +2,30 @@ import React from "react";
 import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 
-const Demo = () => {
+const Demo = props => {
+  const list = props.modules.map((module,index) => {
+    return(
+      <tr key={index}>
+        <th>{module.name}</th>
+        <th>{module.noLectures}</th>
+        <th>{module.noPracticals}</th>
+      </tr>
+    );
+  });
   return (
-      <h1>TODO</h1>
+    <>
+    <h2>{props.course}</h2>
+    <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+  </>
   );
 };
 
